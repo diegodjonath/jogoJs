cc37db636d8097d20face1ba42792e630c6a9161
diegodjonath <diegoalves44@hotmail.com>
2021-09-03 23:04:45 -0300

    Jogo em JS

---
+++ jogo.js
@@ -0,0 +1,42 @@
+var random = 0;
+var plays = 1;
+var number = 0;
+
+generateRandom();
+
+//play();
+function play() { 
+    number = prompt(plays + 'º Tentativa, adivinhe o número?');
+
+    checkResult();
+}
+
+function checkResult() {
+    console.log(number + '==' + random);
+
+    if (number == random) {
+        alert('Acertou. Parabénss!!');
+
+        resetGame();
+
+    } else if (plays >= 3) {
+        alert('Game Over');
+
+        resetGame();
+
+    } else {
+        plays ++;
+
+        play();
+    }
+}
+
+function generateRandom () {
+    random = Math.floor(Math.random() * 10);
+}
+
+function resetGame () {
+    plays = 1;
+
+    generateRandom();
+}
